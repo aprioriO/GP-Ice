6a6f93fc78711940a293dbd08e853ccbdc832218
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-30 19:58:23 +0000

    Inventories controller refactoring

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,13 +1,17 @@
 class InventoriesController < ApplicationController
+  before_action :set_van, only: [:index, :show, :new, :create]
+  before_action :set_products, only: [:new, :create]
+  before_action :set_product, only: [:new, :create]
+  before_action :build_inventory, only: :create
 
   def index
-    @van = Van.find(params[:van_id])
+    # @van = Van.find(params[:van_id])
     @inventories = @van.inventories
     @user = current_user
   end
 
   def show
-    @van = Van.find(params[:van_id])
+    # @van = Van.find(params[:van_id])
     @inventory = @van.inventories.find_by(id: params[:id])
     if @inventory.nil?
       redirect_to van_inventories_path(@van), alert: 'Inventory not found'
@@ -15,20 +19,11 @@ class InventoriesController < ApplicationController
   end
 
   def new
-    @van = Van.find(params[:van_id])
     @inventory = Inventory.new
     @inventory.van = @van
-    @products = Product.all
   end
 
   def create
-    @van = Van.find(params[:van_id])
-    @product = Product.find(params[product_id: :product_id])
-    @inventory = @van.inventories.find_or_initialize_by(product: @product)
-    @inventory.quantity_avilable = params[:quantity_available]
-    @inventory.price = params[:price]
-    # @inventory.user = current_user
-    @inventory.van = @van
     if @inventory.save
       flash[:notice] = "Inventory created"
       redirect_to van_inventories_path(@inventory.van)
@@ -36,4 +31,30 @@ class InventoriesController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:product_id, :quantity_available, :price)
+  end
+
+  def set_van
+    @van = Van.find(params[:van_id])
+  end
+
+  def set_products
+    @products = Product.all
+  end
+
+  def set_product
+    @product = Product.find_or_initialize_by(id: params[:product_id])
+  end
+
+  def build_inventory
+    @inventory = Inventory.new(inventory_params)
+    @inventory.price = params[:price]
+    @inventory.user = current_user
+    @inventory.van = @van
+  end
+
 end
