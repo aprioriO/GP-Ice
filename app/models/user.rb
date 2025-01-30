7022dc1ae1db28005f517834cbb089b463ab739f
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-30 12:34:10 +0000

    User model: user has one van

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
-  has_many :vans
+  has_one :van
   has_many :orders
   has_many :favourites
   has_many :favourite_vans, through: :favourites, source: :van
