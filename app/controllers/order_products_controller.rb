08b772c781426f56475c6b08459cc764aac48271
Yazid Ijeal <yazuni001@gmail.com>
2025-01-31 14:54:08 +0000

    mycart/checkout working

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -3,14 +3,14 @@ class OrderProductsController < ApplicationController
   before_action :set_order
 
   def show
-    @cart_items = @order.order_products.includes(:product)
+    @cart_items = @order.order_products.includes(:product) if @order
     render "carts/show"
   end
 
   def create
     @product = Product.find(params[:product_id])
     @order_product = @order.order_products.find_or_initialize_by(product: @product)
-    @order_product.quantity_ordered = params[:quantity_ordered]
+    @order_product.quantity_ordered = params[:order][:quantity_ordered].to_i if params[:order][:quantity_ordered].present?
 
     if @order_product.save
       redirect_to van_cart_path(@van), notice: "Product added to cart."
@@ -21,12 +21,8 @@ class OrderProductsController < ApplicationController
 
   def destroy
     @order_product = @order.order_products.find(params[:id])
-
-    if @order_product.destroy
-      redirect_to van_cart_path(@van), notice: "Product removed from cart."
-    else
-      redirect_to van_cart_path(@van), alert: "Failed to remove product."
-    end
+    message = @order_product.destroy ? "Product removed from cart." : "Failed to remove product."
+    redirect_to van_cart_path(@van), notice: message
   end
 
   private
@@ -36,6 +32,6 @@ class OrderProductsController < ApplicationController
   end
 
   def set_order
-    @order = Order.find_or_create_by(user: current_user, status: "pending", van: @van)
+    @order = Order.find_or_create_by(user: current_user, van: @van, confirmed_status: false)
   end
 end
