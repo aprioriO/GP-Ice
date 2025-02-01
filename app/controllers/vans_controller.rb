f396147bd3d86188513bf5a8147c501bbc53f3c8
Olena Hulakova <hulakova.olena@gmail.com>
2025-02-01 21:12:59 +0000

    Fix link to Cart in navbar

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -16,7 +16,7 @@ class VansController < ApplicationController
     @van = current_user.build_van(van_params) # Allows each user to have only one van
     if @van.save
       flash[:notice] = "You are now a van driver!"
-      redirect_to @van.inventories.new
+      redirect_to @van
     else
       render :new
     end
