c81869e4b6a6025815c83da381dc148f2ce23e01
Olena Hulakova <hulakova.olena@gmail.com>
2025-02-03 13:52:18 +0000

    Route for new favourite

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     patch "products/:id" => "products#update"
     resources :orders, only: %i[show create update destroy]
     resources :inventories
+    get "favourite" => "favourites#new", as: :favourite
     post "favourite" => "favourites#create", as: :favourite
   end
 
