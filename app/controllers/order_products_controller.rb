6822f2ede1e9205e6f7234a86f75ef8dbc3347bb
Yazid Ijeal <yazuni001@gmail.com>
2025-02-04 10:09:52 +0000

    redirect-add-to-cart-button-to-store

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -7,17 +7,21 @@ class OrderProductsController < ApplicationController
   end
 
   def create
-    product = Product.find(params[:product_id])
-    van = product.inventories.first.van
+    @product = Product.find(params[:product_id])
+    @van = @product.inventories.first.van
 
-    order = Order.find_or_create_by(user: current_user, van: van, confirmed_status: false, status: "pending")
-    order_product = order.order_products.find_or_initialize_by(product: product)
+    @order = Order.find_or_create_by(user: current_user, van: @van, confirmed_status: false, status: "pending")
+    @order_product = @order.order_products.find_or_initialize_by(product: @product)
 
-    order_product.quantity_ordered = params[:order][:quantity_ordered].to_i if params[:order][:quantity_ordered].present?
+    @order_product.quantity_ordered = params[:order][:quantity_ordered].to_i if params[:order][:quantity_ordered].present?
 
-    message = order_product.save ? "Product added to cart." : "Failed to add product."
-    redirect_to cart_path, notice: message
+    if @order_product.save
+      redirect_to van_path(@van), notice: "Product added to cart."
+    else
+      redirect_to van_path(@van), alert: "Failed to add product."
+    end
   end
+
 
   def destroy
     order_product = OrderProduct.find(params[:id])
